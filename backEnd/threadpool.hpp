dd239d9b9efcb1cf8eed7c85e72e24db7aab0237
Bryce Hart <bryce.hart23@gmail.com>
2026-03-20 10:13:22 -0400

    added threadpool class

--- backEnd/threadpool.hpp
+++ backEnd/threadpool.hpp
@@ -1,1 +1,54 @@
+#pragma once
 #include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+#include <queue>
+#include <vector>
+
+class ThreadPool {
+public:
+    static constexpr int THREAD_COUNT = 10;
+
+    ThreadPool() : stop(false) {
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            workers.emplace_back([this] {
+                while (true) {
+                    std::function<void()> task;
+                    {
+                        std::unique_lock<std::mutex> lock(queueMutex);
+                        cv.wait(lock, [this] { return stop || !tasks.empty(); });
+                        if (stop && tasks.empty()) return;
+                        task = std::move(tasks.front());
+                        tasks.pop();
+                    }
+                    task();
+                }
+            });
+        }
+    }
+
+    ~ThreadPool() {
+        {
+            std::unique_lock<std::mutex> lock(queueMutex);
+            stop = true;
+        }
+        cv.notify_all();
+        for (auto& t : workers) t.join();
+    }
+
+    void enqueue(std::function<void()> task) {
+        {
+            std::unique_lock<std::mutex> lock(queueMutex);
+            tasks.push(std::move(task));
+        }
+        cv.notify_one();
+    }
+
+private:
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+    std::mutex queueMutex;
+    std::condition_variable cv;
+    bool stop;
+};
